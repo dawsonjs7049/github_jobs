2c9003b423ba693dee0b4d2ae33e14f81775f643
JakeTheCodingSnake <jake906@charter.net>
2020-07-21 13:32:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import useFetchJobs from './useFetchJobs';
+import { Container, Spinner } from 'react-bootstrap'
+import Job from './Job'
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm'
+
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name 
+    const value = e.target.value 
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value}
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container className="my-4">
+        <h1 className="mb-4">GitHub Jobs</h1>
+        <SearchForm params={params} onParamChange={handleParamChange}></SearchForm>
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+        {loading && 
+          <Spinner animation="border" role="status" variant="primary" className="mt-3">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        }
+        {error && <h1>Error, try refreshing.</h1>}
+        {jobs.map(job => {
+          return <Job key={job.id} job={job}></Job>
+        })}
+        {!loading && 
+          <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+        }
+      </Container>
     </div>
   );
 }
